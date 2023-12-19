2747196e141898f8b0fa0e5b59f00f226f36605e
Fressia Barrios <78377631+barrios2@users.noreply.github.com>
2023-12-19 11:04:26 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,10 @@
 import { quizData } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 
-const saveState = (state) => {
-  localStorage.setItem('quizState', JSON.stringify(state));
-};
-
-const loadState = () => {
-  const savedState = localStorage.getItem('quizState');
-  return savedState ? JSON.parse(savedState) : null;
-};
-
-const resetState = () => {
-  localStorage.removeItem('quizState');
-};
-
 const loadApp = () => {
-  const savedState = loadState();
+  quizData.currentQuestionIndex = 0;
 
-  if (savedState) {
-    quizData.currentQuestionIndex = savedState.currentQuestionIndex;
-    // Load the appropriate page based on the saved state
-    if (savedState.onResultsPage) {
-      initResultsPage();
-    } else {
-      initQuestionPage();
-    }
-  } else {
-    initWelcomePage();
-  }
+  initWelcomePage();
 };
 
 window.addEventListener('load', loadApp);
