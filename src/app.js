c819e696c44b3930f16f0ef10115956653e247a8
Gabriele Salvo <72619479+gabsalvo@users.noreply.github.com>
2023-12-19 11:09:08 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ import { initWelcomePage } from './pages/welcomePage.js';
 
 const loadApp = () => {
   quizData.currentQuestionIndex = 0;
-
   initWelcomePage();
 };
 
 window.addEventListener('load', loadApp);
-window.addEventListener('beforeunload', (event) => {.
+
+window.addEventListener('beforeunload', (event) => {
+  // Cancel the event as stated by the standard.
   event.preventDefault();
+  // Chrome requires returnValue to be set.
   event.returnValue = 'Are you sure you want to leave? Your progress might be lost.';
 });
+
