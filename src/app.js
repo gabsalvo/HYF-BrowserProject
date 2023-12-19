74443689576bd82fc7cfc575b55aa6ee4a60dfba
Gabriele Salvo <72619479+gabsalvo@users.noreply.github.com>
2023-12-19 11:16:38 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 import { quizData } from './data.js';
-import { initWelcomePage } from './pages/welcomePage.js';
+import { initWelcomePage, initQuestionPage, initResultPage } from './pages';
 
 const loadApp = () => {
-  quizData.currentQuestionIndex = 0;
-  initWelcomePage();
+  // Check if there's saved quiz data in localStorage
+  const storedData = JSON.parse(localStorage.getItem('quizData'));
+
+  if (storedData) {
+    // Restore the saved quiz state
+    quizData.currentQuestionIndex = storedData.currentQuestionIndex;
+
+    // Check if the user was in the middle of a quiz or on the results page
+    if (storedData.currentQuestionIndex < quizData.questions.length) {
+      // User was in the middle of the quiz
+      initQuestionPage();
+    } else {
+      // User had finished the quiz and was on the results page
+      initResultPage();
+    }
+  } else {
+    // No saved data, start at the welcome page
+    initWelcomePage();
+  }
 };
 
 window.addEventListener('load', loadApp);
+
 
