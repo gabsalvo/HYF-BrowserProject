7e4490239dc4f329a8c1fc4abfc3893b113c6820
Gabriele Salvo <72619479+gabsalvo@users.noreply.github.com>
2023-12-19 10:53:02 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
 import { quizData } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 
-const loadApp = () => {
-  quizData.currentQuestionIndex = 0;
+const saveState = (state) => {
+  localStorage.setItem('quizState', JSON.stringify(state));
+};
 
-  initWelcomePage();
+const loadState = () => {
+  const savedState = localStorage.getItem('quizState');
+  return savedState ? JSON.parse(savedState) : null;
+};
+
+const resetState = () => {
+  localStorage.removeItem('quizState');
+};
+
+const loadApp = () => {
+  const savedState = loadState();
+
+  if (savedState) {
+    quizData.currentQuestionIndex = savedState.currentQuestionIndex;
+    // Load the appropriate page based on the saved state
+    if (savedState.onResultsPage) {
+      initResultsPage();
+    } else {
+      initQuestionPage();
+    }
+  } else {
+    initWelcomePage();
+  }
 };
 
 window.addEventListener('load', loadApp);
