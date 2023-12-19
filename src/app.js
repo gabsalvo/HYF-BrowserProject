092d07bd4d3ea2b0fa0bf3e2ff36b4c114613519
Gabriele Salvo <72619479+gabsalvo@users.noreply.github.com>
2023-12-19 11:06:15 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,12 +10,9 @@ const loadApp = () => {
 window.addEventListener('load', () => {
   if (sessionStorage.getItem("isReload")) {
     console.log("Page was reloaded");
-    // Handle the reload case, maybe redirect to a specific page or perform some action
-    // For example, you could call initQuestionPage() or initResultsPage() based on saved state
   } else {
     sessionStorage.setItem("isReload", true);
     console.log("Page loaded for the first time");
-    // Normal load handling
     initWelcomePage();
   }
 });
